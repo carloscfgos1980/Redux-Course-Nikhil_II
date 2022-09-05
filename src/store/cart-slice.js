f41584714d4b6368b7482670905d54246842a50d
CarlosCubNed <kuva5008@gmail.com>
2022-09-05 06:13:38 +0200

    + - button working

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -26,7 +26,17 @@ const cartSlice = createSlice({
                 state.totalQuantity++; // increase the total quantity
             }
         },
-        removeFromCart(state, action) { },
+        removeFromCart(state, action) {
+            const id = action.payload;
+
+            const existingItem = state.itemsList.find((item) => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.itemsList = state.itemsList.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice -= existingItem.price
+            }
+        },
         setShowCart(state) {
             state.showCart = !state.showCart; // allows to swith from true to false everytime we call this function. USeful to show the info in the DOM
         }
