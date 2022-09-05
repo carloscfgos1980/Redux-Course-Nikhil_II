d33fae31b615a36c1736a21ecb382ae819b2a937
CarlosCubNed <kuva5008@gmail.com>
2022-09-05 07:30:45 +0200

    send info to back-end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
 
 function App() {
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
-
+  const cart = useSelector(state => state.cart);
   const isLoggIn = useSelector((state) => state.auth.isLoggIn);
-  console.log(isLoggIn)
+
+  // useEffect for fetching an API
+  useEffect(() => {
+    fetch('https://redux-http-aa16a-default-rtdb.firebaseio.com/cartItems.json', // Given name to the collection we are going to create
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      })
+  }, [cart])
   return (
     <div className="App">
       {!isLoggIn && <Auth />}
